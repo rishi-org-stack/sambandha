9944b66c5771376d13d1434ed449d1057b1ad1ef
rishi_muni <rishi.jha_xt@angkas.com>
2024-01-12 17:59:31 +0530

    feat(protocol): initial impl of prrotocol data types

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,41 @@
-use std::{
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-};
-
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
-    }  
+struct SendType {
+    sender_id: Vec<u8>,
+    reciever_id: Vec<u8>,
+    pub_key: Vec<u8>,
+    content: Content,
+    message_id: Vec<u8>,
+    sent_on: u128,
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buf: Vec<u8> = vec![];
-    stream.read(&mut buf).expect("not ok");
-    let req = String::from_utf8(buf).unwrap();
-    println!("req={:?}", req);
-    let ok: Vec<&str> = req.lines().collect();
-    println!("{:?}", ok);
-    // loop {
-    //     let buf_reader = BufReader::new(&mut stream);
-    //     let http_request: Vec<_> = buf_reader
-    //         .lines()
-    //         .map(|result| result.expect("not ok"))
-    //         .take_while(|line| !line.is_empty())
-    //         .collect();
-    //     println!("Request: {:#?}", http_request);
+struct Content {
+    size: u16,
+    data: Vec<u128>,
+}
+enum SentMsgState {
+    Delivered,
+    Read,
+}
 
-    //     stream.write(b"ok\n").expect("failed to write");
-    //}
- }
+enum UserStatus {
+    Online,
+    Offline,
+}
+struct SentAck {
+    message_id: Vec<u8>,
+    state: SentMsgState,
+    ack_on: u128,
+}
+
+struct StatusUpdate {
+    user_id: Vec<u8>,
+    status: UserStatus,
+    on: u128,
+}
+
+struct Message {
+    sender_id: Vec<u8>,
+    message_id: Vec<u8>,
+    content: Content,
+}
+
+fn main() {}
